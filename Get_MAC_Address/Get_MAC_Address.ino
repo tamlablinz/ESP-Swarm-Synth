987c98c936d32b332ba63b7c2c719400b80f46ee
ullarauter <106682486+ullarauter@users.noreply.github.com>
2025-04-29 17:01:09 +0200

    Create Get_MAC_Address.ino

---
+++ Get_MAC_Address/Get_MAC_Address.ino
@@ -0,0 +1,15 @@
+#include <WiFi.h>
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_MODE_STA); 
+  Serial.print("ESP32 MAC Address: ");
+  Serial.println(WiFi.macAddress());
+}
+
+void loop() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_MODE_STA); 
+  Serial.print("ESP32 MAC Address: ");
+  Serial.println(WiFi.macAddress()); 
+}
